a30da9432ab55ae61fee9444ae57e2fa37d6276c
Novastatis <inicexdi@gmail.com>
2022-06-02 07:13:52 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #include<iostream>
 #include<glad/glad.h>
 #include<glfw/glfw3.h>
-using namespace std;
-
-int termination(const char* reason) {
-	cout << reason;
-	glfwTerminate();
-	return -1;
-}
 
 const char* vertex_shader_source = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -23,14 +17,27 @@ const char* fragment_shader_source = "#version 330 core\n"
 "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
 "}\n\0";
 
-int main(void) {
-	unsigned short width = 800;
-	unsigned short height = 600;
+void framebuffer_resize_callback(GLFWwindow* window, int fbWidth, int fbHeight) {
+	glViewport(0, 0, fbWidth, fbHeight);
+}
 
-	const GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		 0.0f,  0.5f * float(sqrt(3)) * 2/3, 0.0f
+int main(void) {
+	int width = 800;
+	int height = 600;
+	
+	GLfloat vertices[] = {
+		-0.5f,    -0.5f * float(sqrt(3))     / 3, 0.0f,
+		 0.5f,    -0.5f * float(sqrt(3))     / 3, 0.0f,
+		 0.0f,     0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+		-0.5f / 2, 0.5f * float(sqrt(3))     / 6, 0.0f,
+		 0.5f / 2, 0.5f * float(sqrt(3))     / 6, 0.0f,
+		 0.0f,    -0.5f * float(sqrt(3))     / 3, 0.0f
+	};
+
+	GLuint indicies[] = {
+		0, 3, 5,
+		3, 2, 4,
+		5, 4, 1
 	};
 
 	glfwInit();
@@ -39,12 +46,14 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", NULL, NULL);
-	if (window == NULL) 
-		termination("[error] failed to create window\n");
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+
 	glfwMakeContextCurrent(window);
-	
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
 	gladLoadGL();
-	glViewport(0, 0, width, height);
 
 	//SHADERS
 	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -64,19 +73,27 @@ int main(void) {
 	glDeleteShader(fragment_shader);
 
 	//BUFFERS
-	GLuint VBO, VAO; //Vertex Buffer Object & Vertex Array Object
+	GLuint VAO; //Vertex Array Object
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	
 	glBindVertexArray(VAO);
+	
+	GLuint VBO; //Vertex Buffer Object
+	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+	GLuint EBO; //Element Buffer Object
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -84,15 +101,15 @@ int main(void) {
 
 		glUseProgram(shader_program);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteShader(shader_program);
 	glfwDestroyWindow(window);
 	glfwTerminate();
